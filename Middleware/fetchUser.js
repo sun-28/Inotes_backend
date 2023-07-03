247056856df1f0a10e7c96c699465a71d75c9fab
Sun28 <sunpahwa28@gmail.com>
2023-07-03 21:05:28 +0530

    updates jwt_secret in middleware

--- Middleware/fetchUser.js
+++ Middleware/fetchUser.js
@@ -1,5 +1,5 @@
 const jwt= require('jsonwebtoken')
-const JWT_SECRET= "Ss@2809"
+const JWT_SECRET= process.env.JWT_SECRET
 
 const fetchUser= (req,res,next) =>{
     const token = req.header('auth-token');
